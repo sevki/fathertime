2f5d78bfed529dbc972919756cdcabfa91655986
Sevki Hasirci <s@sevki.org>
2014-10-27 13:03:16 +0200

    Update fathertime.go

--- fathertime.go
+++ fathertime.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package fathertime converts time.Time in to human readable string
 package fathertime
 
 import (
